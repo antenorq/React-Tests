21193c9ead68e0f855082cdc84a90e6673d195f7
Antenor Queiroz <antenorq@gmail.com>
2018-02-08 18:55:31 -0300

    first commit

---
+++ app/App.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+ReactDom.render(<h1>testeeeeewrwerwee</h1>,document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = 
+{
+  entry: "./app/App.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react'] 
+        }
+      }
+    ]
+  } 
+}
